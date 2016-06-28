35cf4a49902c2020c4338904c4e244b7cf5e8ac1
nes <vukasin.nesovic@gmail.com>
2016-06-28 09:28:11 +0200

    server port

--- server.js
+++ server.js
@@ -157,6 +157,6 @@ app.get('/knockout-phase/predictions/:id', function (request, response) {
 //     });
 // });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log('Example app listening on port 3000!');
 });
